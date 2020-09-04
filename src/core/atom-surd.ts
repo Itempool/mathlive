Itempool/mathlive

commit fb3b0d87e6862cefd8e383e59f1235e9469fa035

fix: `import.meta` causes issues #508

src/core/atom-surd.ts
@@ -67,7 +67,7 @@ registerAtomType('surd', (context: Context, atom: Atom): Span[] => {
 
     const body = makeVlist(context, [inner, lineClearance, line, ruleWidth]);
 
-    if (!atom.index) {
+    if (typeof atom.index === 'undefined') {
         return [atom.bind(context, makeSpan([delim, body], 'sqrt', 'mord'))];
     }
 

src/core/definitions-functions.ts
@@ -103,7 +103,7 @@ defineFunction(
     },
     (name, _parent, atom, emit) => {
         let args = '';
-        if (atom.index) {
+        if (typeof atom.index !== 'undefined') {
             args += `[${emit(atom, atom.index)}]`;
         }
         args += `{${emit(atom, atom.body as Atom[])}}`;

commit a5839a3808faff1df3c20f8c8e84ff85571757e8

Fix issue with spacing around radicals
I don't fully understand what's going on here, but the issue appears to have been introduced in fb3b0d87e6862cefd8e383e59f1235e9469fa035 when changing this check from `if (!atom.index)` to  checking `if (typeof atom.index === 'undefined')`. It seems that `atom.index` is actually `null` in the relevant case, so this change ought to handle the case of `null` or `undefined` but not `0`, which was presumably the intent of the previous change that broke it. I dunno.

src/core/atom-surd.ts
@@ -68,7 +68,7 @@ registerAtomType('surd', (context: Context, atom: Atom): Span[] => {
 
     const body = makeVlist(context, [inner, lineClearance, line, ruleWidth]);
 
-    if (typeof atom.index === 'undefined') {
+    if (atom.index == null) {
         return [atom.bind(context, makeSpan([delim, body], 'sqrt', 'mord'))];
     }
 

src/core/definitions-functions.ts
@@ -103,7 +103,7 @@ defineFunction(
     },
     (name, _parent, atom, emit) => {
         let args = '';
-        if (typeof atom.index !== 'undefined') {
+        if (atom.index != null) {
             args += `[${emit(atom, atom.index)}]`;
         }
         args += `{${emit(atom, atom.body as Atom[])}}`;